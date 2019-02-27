07056ed024578fb50f5cb4fc6c9339af4cdee7d2
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-02-27 14:26:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -174,24 +174,6 @@ if (message.content.startsWith(prefix + "ban")) {
 message.channel.send(`** ${user.tag} banned from the server.**  `).then(message => message.delete(10000))
 }
 });
------------------------------------------------------------------------------------------------------------------------
-اقتراح
-client.on('message', message => {
-	 var prefix = "#";
-  if(message.content.startsWith(prefix + "اقتراح")) {
-  await  message.channel.send(`اكتب اقتراحك الان`)
-    let filter = m => m.author.id === message.author.id
-      var text = '';
-        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
-          .then(co => {
-            text = co.first().content
 
-              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
-                client.channels.get("495359919529263116").send(`${message.author.username}'s sug => ${text}`)
-
-              })
-            }
-          })
-});
 
 client.login(process.env.BOT_TOKEN);
