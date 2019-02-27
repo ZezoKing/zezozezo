5e21f1f78bb275d5bae23bce03070a1b2ebe1c84
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-02-27 14:11:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` اكتب اي شي هنا  `,"http://twitch.tv/S-F")
+client.user.setGame(` Created Bot By Zezo  `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -25,6 +25,24 @@ client.user.setGame(` اكتب اي شي هنا  `,"http://twitch.tv/S-F")
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
+});
+
+client.on('message', message => {
+	 var prefix = "#";
+  if(message.content.startsWith(prefix + "اقتراح")) {
+  await  message.channel.send(`اكتب اقتراحك الان`)
+    let filter = m => m.author.id === message.author.id
+      var text = '';
+        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
+          .then(co => {
+            text = co.first().content
+
+              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
+                client.channels.get("495359919529263116").send(`${message.author.username}'s sug => ${text}`)
+
+              })
+            }
+          })
 });
 
 client.login(process.env.BOT_TOKEN);
