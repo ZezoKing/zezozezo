651f260ab8cf2d08fd07ef2662bd94b1c6fcad27
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-03-01 07:06:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,95 @@
-client.on("message", msg =>{
-var prefix = '#',//البرفكس
-if(msg.content.startsWith(`${prefix}top-servers`)){ // الامر (topserver)
-  let noTop = msg.content.split(" ")[1];
-  const top = client.guilds.sort((a,b)=>a.memberCount-b.memberCount).array().reverse()
-  if(!noTop) noTop = 10;
-  if(isNaN(noTop)) noTop = 10;
-  if(noTop <= 0) noTop = 10;
-  if(noTop > top.length) noTop = top.length;
-  let serveremmbed = new Discord.RichEmbed();
-  for(let i = 0; i < noTop; i++){
-  serveremmbed.addField(`**${top[i].name}** : ${top[i].memberCount}`," ‎ ‎ ‎ ‎ ‎ ‎‎ ‎ ‎ ‎");
-  }
-  serveremmbed.setColor('#36393e')
-  serveremmbed.setTitle(`**Top ${noTop} Servers**`);
-  serveremmbed.setThumbnail(msg.author.displayAvatarURL);
-  serveremmbed.setTimestamp();
-  serveremmbed.setFooter(client.user.username,client.user.displayAvatarURL);
-  msg.channel.send(serveremmbed);
-}});
-
-
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require("fs");
+var Canvas = require('canvas');
+var jimp = require('jimp');
+const moment = require('moment');
+var prefix = "+";
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` اكتب اي شي هنا  `,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online ')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+ 
+client.on('message', message => {
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith('+ping')) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('RANDOM')
+                        .addField('**Time Taken:**',msg + " ms 📶 ")
+                        .addField('**WebSocket:**',api + " ms 📶 ")
+         message.channel.send({embed:embed});
+                        }
+                    });
+ 
+client.on('message', message => {
+              if(!message.channel.guild) return;
+    if(message.content.startsWith('+bc')) {
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "SkelBot";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لأرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+ 
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`☑ |   ${message.guild.members.size} يتم أرسال البرودكاست الى عضو `).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('البرودكاست') .addField('السيرفر', message.guild.name) .addField('المرسل', message.author.username)
+       .addField('الرساله', args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**تم الغاء البرودكاست**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+    })
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 client.login(process.env.BOT_TOKEN);
