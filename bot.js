857e21343c2aabc48cfe67e1e4ed29a08ed13b6c
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-05-01 17:41:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ var server = "509037653040300053";
 var channel = "509037812666859540";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('** زيزو رجع **')
+    client.guilds.get(server).channels.get(channel).send('** Zezo Is Back **')
     },305);
 }) 
 
