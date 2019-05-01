c84591ce2f42d4ee6376053036d9e3bc152ec821
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-05-01 17:41:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', async() => {
-var server = "509037653040300053"; 
-var channel = "509037812666859540";
+var server = "573149917103783976"; 
+var channel = "573151578479919115";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('** زيزو رجع **')
     client.guilds.get(server).channels.get(channel).send('** Zezo Is Back **')
