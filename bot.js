6afae621a9f616c783fdab4dbd7ef7da9e98590b
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-09-28 12:04:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', async() => {
-var server = "509037653040300053"; 
-var channel = "509037812666859540";
+var server = "624906460761030656"; 
+var channel = "627422703103508490";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send(' #credits  ')
     },305);
