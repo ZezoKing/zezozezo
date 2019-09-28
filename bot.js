0ce7a07eb56bb5be3469dbb930ebaa7b537fb7e3
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-09-28 12:01:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,12 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on("message", message => {
-    var prefix = "=";
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
-        var msg;
-        msg = parseInt();
-      
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-        title: "Done | تــم",
-        color: 0x06DF00,
-        description: "تم مسح الرسائل بنجاح",
-        footer: {
-          text: "Desert Bot"
-        }
-      }}).then(msg => {msg.delete(3000)});
-         }                 
-});
-
+client.on('ready', async() => {
+var server = "509037653040300053"; 
+var channel = "509037812666859540";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send(' #credits  ')
+    },305);
+}) 
 
 
 client.login(process.env.BOT_TOKEN);
