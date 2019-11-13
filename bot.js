ad1873c9f410bfd9deefc6ff655cedb051dd2403
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-11-13 11:41:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require("fs");
+var Canvas = require('canvas');
+var jimp = require('jimp');
+const moment = require('moment');
+var prefix = "+";
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` تكوتا  `,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online ')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
 setInterval(() => {
     let textchannel = client.channels.find(channel => channel.id === '644073141240594442'); // ايدي الروم الي تبي يرسل فيه 
 
@@ -5,3 +37,5 @@ setInterval(() => {
 }, 240000); // 240000 = 4 دقايق
 
 client.login(process.env.BOT_TOKEN);
+
+
