727b056661844980e6f9f9121d60365e68453cca
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-11-19 20:06:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', async() => {
 var server = "619139460092788746"; 
-var channel = "646390015563137025";
+var channel = "646358719457656842";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('** سلوى اكس برو   **')
     },305);
